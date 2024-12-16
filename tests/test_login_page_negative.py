a26c4108c353ab7d469316fd7f93add6668581b0
romanstr-code <romanstrogoteanu1990@gmail.com>
2024-12-16 12:56:00 +0000

    Created conftest file with pytest fixtures. added parametrize mark in test_negative_page

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -1,20 +1,42 @@
 import time
 
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 class TestNegativeScenario:
 
-
     @pytest.mark.login
     @pytest.mark.negative
-    def teste_negative_username(self):
-        # Open Page
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    @pytest.mark.parametrize("username, password, expected_error_message",
+                             [("incorrectUser", "Password123", "Your username is invalid!"),
+                              ("student", "incorrectPassword", "Your password is invalid!")])
+    def teste_negative_login(self, driver, username, password, expected_error_message):
+        # Go to Page
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type username incorrectUser into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys(username)
+
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys(password)
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should"
+
+        # Verify error message text is Your username is invalid
+        error_message = error_message_locator.text
+        assert error_message == expected_error_message, "Error message is not expected"
+
+    def teste_negative_username(self, driver):
         # Go to Page
         driver.get("https://practicetestautomation.com/practice-test-login/")
 
@@ -38,3 +60,29 @@ class TestNegativeScenario:
         # Verify error message text is Your username is invalid
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!", "Error message is not expected"
+
+    # Testing the Negative Scenario for Password
+    def test_negative_password(self, driver):
+        # Go to Page
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+
+        # Type password incorrectPassword into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("incorrectPassword")
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed, but it should"
+
+        # Verify error message text is Your password is invalid!
+        error_message = error_message_locator.text
+        assert error_message == "Your password is invalid!", "Error message is not expected"
