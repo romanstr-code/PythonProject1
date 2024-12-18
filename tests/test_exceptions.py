53136683f2a152132d8a2ae63b7658d679a41ed8
romanstr-code <romanstrogoteanu1990@gmail.com>
2024-12-18 19:35:01 +0000

    refactor test_exceptions - waiting for driver for 10sec. row 2 input element is waiting until the presence of the xpath

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class TestExceptions:
 
@@ -12,6 +13,8 @@ class TestExceptions:
         add_button_locator = driver.find_element(By.ID, "add_btn")
         add_button_locator.click()
 
+        wait = WebDriverWait(driver, 10)
+        row_2_input_element = wait.until(ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+
         # Verify Row 2 input field is displayed
-        row_2_input_locator = driver.find_element(By.XPATH, "//div[@id='row2']/input")
-        assert row_2_input_locator.is_displayed(), "Row 2 should be displayed but is not"
+        assert row_2_input_element.is_displayed(), "Row 2 should be displayed but is not"
